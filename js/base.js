3be5b40136e977879159cb90914da2cc47d344a9
acebing <1228984759@qq.com>
2018-05-12 14:40:27 +0800

    Initial commit

---
+++ js/base.js
@@ -0,0 +1,46 @@
+$(function () {
+	// 个人中心
+	$('.UseMouseoverHD').mouseover(function(){
+		$('.yx-cp-siteNavItemBd .UseMouseover').show();	
+		$('.yx-cp-siteNavItemBd .UseMouseover').mouseover(function(){
+			$(this).show();
+		});
+	});
+	$('.UseMouseoverHD').mouseout(function(){
+		$('.yx-cp-siteNavItemBd .UseMouseover').hide();
+		$('.yx-cp-siteNavItemBd .UseMouseover').mouseout(function(){
+			$(this).hide();
+		});
+	});
+	// 客服服务
+	$('.OrderMouseoverHD').mouseover(function(){
+		$('.yx-cp-siteNavItemBd .OrderMouseover').show();	
+		$('.yx-cp-siteNavItemBd .OrderMouseover').mouseover(function(){
+			$(this).show();
+		});
+	});
+	$('.OrderMouseoverHD').mouseout(function(){
+		$('.yx-cp-siteNavItemBd .OrderMouseover').hide();
+		$('.yx-cp-siteNavItemBd .OrderMouseover').mouseout(function(){
+			$(this).hide();
+		});
+	});
+	// 搜索框
+	$('.yx-cp-searchInput').focus(function(){ 
+    $('.yx-cp-m-suggest').show(); 
+	});
+	$('.yx-cp-searchInput').blur(function(){ 
+    $('.yx-cp-m-suggest').hide(); 
+	});
+	// 菜单
+	$('.yx-cp-tabNav-dropdown .yx-cp-topLevel').mouseover(function(){
+		for (var i = 0; i < $(this).length; i++) {
+			consol.log()
+		}
+		// $('.yx-cp-tabNav-dropdown').show();	
+		// alert('sdsd')
+	});
+	$('.yx-cp-tabNav-dropdown .yx-cp-topLevel').mouseout(function(){
+		$('.yx-cp-tabNav-dropdown').hide();
+	});
+});
